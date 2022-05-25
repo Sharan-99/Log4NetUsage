bafb55a56a74cfe12b2922800cab91fd3be6f7e7
Sharan-99 <sharan.ceg2017103591@gmail.com>
2022-05-25 11:38:45 +0530

    first commit

---
+++ Log4NetUsage/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//using Log4NetUsage.LoggerManager;
+namespace Log4NetUsage.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        LoggerManager logger = new LoggerManager();
+        // GET: Home
+        public ActionResult Index()
+        {
+          
+            logger.Info("Index-page started...");
+            return View();
+        }
+    }
+}
---
+++ Log4NetUsage/LoggerManager.cs
@@ -0,0 +1,35 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Xml;
+
+namespace Log4NetUsage
+{
+    public class LoggerManager
+    {
+        private static readonly LoggerManager _instance = new LoggerManager();
+        protected ILog monitoringLogger;
+        protected static ILog debugLogger;
+
+        public LoggerManager()
+        {
+            monitoringLogger = LogManager.GetLogger("MonitoringLogger");
+            debugLogger = LogManager.GetLogger("DebugLogger");
+        } 
+
+        /*public LoggerManager(ILog log)
+        {
+            this.monitoringLogger = log;
+        } */
+
+        public void Info(string message)
+        {
+            _instance.monitoringLogger.Info(message);
+        }
+    }
+}
